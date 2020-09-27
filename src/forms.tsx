5128486296c63ca9bfcfab0c4f255bf19dca9f96
iamdi <shvetsovdm@gmail.com>
2020-09-27 17:38:34 +1000

    feat: add subscribe page

--- src/forms.tsx
+++ src/forms.tsx
@@ -5,5 +5,12 @@ export default {
     submitText: 'Join The Better Programmer Movement',
     hideName: true,
     format: 'inline'
+  },
+  DEDICATED_SUBSCRIBE: {
+    formId: '1453400',
+    template: 'Clare',
+    submitText: 'Sign Me Up',
+    hideName: true,
+    format: 'inline'
   }
 }
