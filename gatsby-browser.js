e02ea1a7e0249ce38ef6bfa6b6c014555d05ea53
iamdi <shvetsovdm@gmail.com>
2020-10-09 12:47:59 +1000

    feat: add firebase gatsby plugin & setup analytics

---
+++ gatsby-browser.js
@@ -0,0 +1,5 @@
+import 'firebase/analytics'
+import firebase from "gatsby-plugin-firebase"
+
+// Initialize Firebase Analytics
+firebase.analytics()
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   siteMetadata: {
     title: 'Dmitry Shvetsov, Upbeat Software Developer and Mentor.',
@@ -30,7 +32,6 @@ module.exports = {
       }
     },
     {
-      // PWA plugin
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Dmitry Shvetsov',
@@ -48,6 +49,32 @@ module.exports = {
         name: 'mdPages',
         path: `${__dirname}/src/md-pages/`
       }
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        // This will be available in the next version of the plugin
+        // features: {
+        //   auth: false,
+        //   database: true,
+        //   firestore: false,
+        //   storage: true,
+        //   messaging: false,
+        //   functions: false,
+        //   performance: true,
+        //   analytics: true,
+        // },
+        credentials: {
+          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.GATSBY_FIREBASE_APP_ID,
+          measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID
+        }
+      },
     },
     {
       resolve: 'gatsby-plugin-sitemap',
