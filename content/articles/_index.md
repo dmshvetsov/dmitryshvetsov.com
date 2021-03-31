9bb4b77359da4a4dfcfab532d3b417e8bc05f0f1
Dmitry Shvetsov <shvetsovdm@gmail.com>
2021-04-01 00:59:35 +1000

    init v8
    using zola & Rust

---
+++ content/articles/_index.md
@@ -0,0 +1,6 @@
++++
+title = "List of articles"
+sort_by = "date"
+template = "articles.html"
+page_template = "article_page.html"
++++
--- gatsby-browser.js
+++
@@ -1,5 +0,0 @@
-import 'firebase/analytics'
-import firebase from "gatsby-plugin-firebase"
-
-// Initialize Firebase Analytics
-firebase.analytics()
