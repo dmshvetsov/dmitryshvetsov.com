2181f8d441d513d98c724586dced543cd94fdc3c
dmshvetsov <shvetsovdm@gmail.com>
2023-04-25 16:05:12 +0300

    fix lint and build issues

--- apps/.eslintrc.cjs
+++ apps/.eslintrc.cjs
@@ -1,14 +1,37 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  ignorePatterns: [
+    '.cache/',
+    '.netlify/',
+    'public/'
+  ],
+  rules: {
+    'no-use-before-define': [0],
+    '@typescript-eslint/no-use-before-define': [1]
+  }
 }
